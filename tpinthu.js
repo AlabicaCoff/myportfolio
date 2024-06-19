7372c0f058f509c1f73d3a1fd300acbcaf8a8b90
Thanathat Pinthu <thanathatp2003@gmail.com>
2024-06-19 22:29:12 +0700

    fix responsive

--- tpinthu.js
+++ tpinthu.js
@@ -16,7 +16,7 @@ const currentYear = new Date().getFullYear();
 dynamicYear.textContent = currentYear;
 
 const dynamicText = document.querySelector(".profile-text-p2 span")
-const words = ["Life", "a Skill", "Fun", "Creative", "the Future", "Everything"];
+const words = ["Life", "a Skill", "Fun", "Creative", "the Future", "Everywhere"];
 
 let wordIndex = 0;
 let charIndex = 0;
@@ -45,3 +45,36 @@ const typeEffect = () => {
 }
 
 typeEffect();
+
+let slideIndex = [1,1,1];
+let slideId = ["mySlides1", "mySlides2", "mySlides3"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
+
+setInterval(function() {
+    plusSlides(1, 0);
+}, 15000);
+
+setInterval(function() {
+    plusSlides(1, 1);
+}, 20000);
+
+setInterval(function() {
+    plusSlides(1, 2);
+}, 25000);
